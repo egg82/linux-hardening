bfd3baa64b9ec69ad07614eff7999d7d963f6d89
egg82 <eggys82@gmail.com>
2020-05-18 13:59:21 -0600

    Moved user functions to users.sh

--- users.sh
+++ users.sh
@@ -6,13 +6,23 @@ echo
 echo "[INFO] Starting user configuration.."
 
 echo
+echo "Non-system users:"
+eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1
+
+echo
 echo "Root users:"
 getent group | grep 'x:0:' /etc/passwd | cut -d: -f1 | tr '\n' ','
+
+echo
 echo
 echo "Sudoers:"
 getent group root wheel adm admin | cut -d: -f4 | tr -d '\n'
 
 echo
+echo
+echo "Users with shells:"
+getent passwd | awk -F/ '$NF != "nologin" && $NF != "false" && $NF != "sync"' | cut -d: -f1
+
 echo
 echo "[INFO] Locking accounts.."
 passwd -l root
