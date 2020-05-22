0336e87f6c56abca73348a4409148ea1c4aceb1c
egg82 <eggys82@gmail.com>
2020-05-21 20:38:38 -0600

    Convenience changes

--- simple.sh
+++ simple.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# Ensure IFS exists and is valid
+IFS=" "
+
 # Ensure we are root
 if [ "$(id -u)" != "0" ]
 then
   sudo "$0" "$@"
   exit $?
 fi
-
-IFS=" "
 
 # What OS are we running?
 OS_TYPE=""
@@ -21,7 +22,7 @@ else
   INSTALLER="yum"
 fi
 
-read -p "Users to password change: " -r USERS
+read -p "Users to password change (eg. root,admin,test): " -r USERS
 for i in ${USERS//,/$IFS}
 do
   passwd "$i"
@@ -43,6 +44,8 @@ echo
 read -p "Users to kick: " -r USERS
 for i in ${USERS//,/$IFS}
 do
+  pkill -15 -u "$i"
+  sleep 2
   pkill -9 -u "$i"
 done
 
@@ -51,27 +54,27 @@ done
 FILE=/etc/ssh/sshd_config
 if [ -f $FILE ]
 then
-  grep -qoP '^(#\s*)?PermitEmptyPasswords' $FILE && sed -i -E 's/^(#\s*)?PermitEmptyPasswords.*/PermitEmptyPasswords no/' $FILE || echo 'PermitEmptyPasswords no' >> $FILE
-  grep -qoP '^(#\s*)?PermitUserEnvironment' $FILE && sed -i -E 's/^(#\s*)?PermitUserEnvironment.*/PermitUserEnvironment no/' $FILE || echo 'PermitUserEnvironment no' >> $FILE
-  grep -qoP '^(#\s*)?PrintLastLog' $FILE && sed -i -E 's/^(#\s*)?PrintLastLog.*/PrintLastLog no/' $FILE || echo 'PrintLastLog no' >> $FILE
-  grep -qoP '^(#\s*)?Protocol' $FILE && sed -i -E 's/^(#\s*)?Protocol.*/Protocol 2/' $FILE || echo 'Protocol 2' >> $FILE
-  grep -qoP '^(#\s*)?IgnoreRhosts' $FILE && sed -i -E 's/^(#\s*)?IgnoreRhosts.*/IgnoreRhosts yes/' $FILE || echo 'IgnoreRhosts yes' >> $FILE
-  grep -qoP '^(#\s*)?RhostsAuthentication' $FILE && sed -i -E 's/^(#\s*)?RhostsAuthentication.*/RhostsAuthentication no/' $FILE || echo 'RhostsAuthentication no' >> $FILE
-  grep -qoP '^(#\s*)?RhostsRSAAuthentication' $FILE && sed -i -E 's/^(#\s*)?RhostsRSAAuthentication.*/RhostsRSAAuthentication no/' $FILE || echo 'RhostsRSAAuthentication no' >> $FILE
-  grep -qoP '^(#\s*)?RSAAuthentication' $FILE && sed -i -E 's/^(#\s*)?RSAAuthentication.*/RSAAuthentication yes/' $FILE || echo 'RSAAuthentication yes' >> $FILE
-  grep -qoP '^(#\s*)?HostbasedAuthentication' $FILE && sed -i -E 's/^(#\s*)?HostbasedAuthentication.*/HostbasedAuthentication no/' $FILE || echo 'HostbasedAuthentication no' >> $FILE
-  grep -qoP '^(#\s*)?LoginGraceTime' $FILE && sed -i -E 's/^(#\s*)?LoginGraceTime.*/LoginGraceTime 120/' $FILE || echo 'LoginGraceTime 120' >> $FILE
-  grep -qoP '^(#\s*)?MaxStartups' $FILE && sed -i -E 's/^(#\s*)?MaxStartups.*/MaxStartups 2/' $FILE || echo 'MaxStartups 2' >> $FILE
-  grep -qoP '^(#\s*)?AllowTcpForwarding' $FILE && sed -i -E 's/^(#\s*)?AllowTcpForwarding.*/AllowTcpForwarding no/' $FILE || echo 'AllowTcpForwarding no' >> $FILE
-  grep -qoP '^(#\s*)?X11Forwarding' $FILE && sed -i -E 's/^(#\s*)?X11Forwarding.*/X11Forwarding no/' $FILE || echo 'X11Forwarding no' >> $FILE
+  sed -i 's/^PermitEmptyPasswords/# PermitEmptyPasswords/' $FILE || echo 'PermitEmptyPasswords no' >> $FILE
+  sed -i 's/^PermitUserEnvironment/# PermitUserEnvironment/' $FILE || echo 'PermitUserEnvironment no' >> $FILE
+  sed -i 's/^PrintLastLog/# PrintLastLog/' $FILE || echo 'PrintLastLog no' >> $FILE
+  sed -i 's/^Protocol/# Protocol/' $FILE || echo 'Protocol 2' >> $FILE
+  sed -i 's/^IgnoreRhosts/# IgnoreRhosts/' $FILE || echo 'IgnoreRhosts yes' >> $FILE
+  sed -i 's/^RhostsAuthentication/# RhostsAuthentication/' $FILE || echo 'RhostsAuthentication no' >> $FILE
+  sed -i 's/^RhostsRSAAuthentication/# RhostsRSAAuthentication/' $FILE || echo 'RhostsRSAAuthentication no' >> $FILE
+  sed -i 's/^RSAAuthentication/# RSAAuthentication/' $FILE || echo 'RSAAuthentication yes' >> $FILE
+  sed -i 's/^HostbasedAuthentication/# HostbasedAuthentication/' $FILE || echo 'HostbasedAuthentication no' >> $FILE
+  sed -i 's/^LoginGraceTime/# LoginGraceTime/' $FILE || echo 'LoginGraceTime 120' >> $FILE
+  sed -i 's/^MaxStartups/# MaxStartups/' $FILE || echo 'MaxStartups 2' >> $FILE
+  sed -i 's/^AllowTcpForwarding/# AllowTcpForwarding/' $FILE || echo 'AllowTcpForwarding no' >> $FILE
+  sed -i 's/^X11Forwarding/# X11Forwarding/' $FILE || echo 'X11Forwarding no' >> $FILE
 
   read -p "Users to allow (eg. root,user1,user2): " -r USERS
-  grep -qoP '^(#\s*)?AllowUsers' $FILE && sed -i -E 's/^(#\s*)?AllowUsers.*/AllowUsers '"${USERS//,/ }"'/' $FILE || echo 'AllowUsers '"${USERS//,/ }"'' >> $FILE
+  sed -i 's/^AllowUsers/# AllowUsers/' $FILE || echo "AllowUsers ${USERS//,/ }" >> $FILE
   if [[ $USERS =~ root ]]
   then
-    grep -qoP '^(#\s*)?PermitRootLogin' $FILE && sed -i -E 's/^(#\s*)?PermitRootLogin.*/PermitRootLogin yes/' $FILE || echo 'PermitRootLogin yes' >> $FILE
+    sed -i 's/^PermitRootLogin/# PermitRootLogin/' $FILE || echo 'PermitRootLogin yes' >> $FILE
   else
-    grep -qoP '^(#\s*)?PermitRootLogin' $FILE && sed -i -E 's/^(#\s*)?PermitRootLogin.*/PermitRootLogin no/' $FILE || echo 'PermitRootLogin no' >> $FILE
+    sed -i 's/^PermitRootLogin/# PermitRootLogin/' $FILE || echo 'PermitRootLogin no' >> $FILE
   fi
 
   systemctl restart ssh.service
@@ -163,15 +166,15 @@ cd /root/portspoof || return
 ./configure && make && make install >/dev/null 2>&1
 cp system_files/improved/etc/init.d/portspoof /etc/init.d/portspoof
 eval "cd $CWD"
-FILE=/etc/init.d/portspoof
 echo
 read -p "Portspoof listen port (eg. 4444): " -r LISTEN_PORT
+FILE=/etc/init.d/portspoof
 sed -i -E 's/\s+-i\s+ ${int}.*//' $FILE
-grep -qoP '^(#\s*)?PS_LISTENPORT' $FILE && sed -i -E 's/^(#\s*)?PS_LISTENPORT.*/PS_LISTENPORT='"$LISTEN_PORT"'/' $FILE || echo 'PS_LISTENPORT='"$LISTEN_PORT"'' >> $FILE
-grep -qoP '^(#\s*)?PS_USER' $FILE && sed -i -E 's/^(#\s*)?PS_USER.*/PS_USER=root/' $FILE || echo 'PS_USER=root' >> $FILE
-grep -qoP '^(#\s*)?PS_ARGUMENTS' $FILE && sed -i -E 's/^(#\s*)?PS_ARGUMENTS.*/PS_ARGUMENTS=\"-p $PS_LISTENPORT -s \/root\/portspoof\/tools\/portspoof_signatures\"/' $FILE || echo 'PS_ARGUMENTS=\"-p $PS_LISTENPORT -s /root/portspoof/tools/portspoof_signatures\"' >> $FILE
+sed -i 's/^PS_LISTENPORT/# PS_LISTENPORT/' $FILE || echo "PS_LISTENPORT=$LISTEN_PORT" >> $FILE # Note the double-quotes here
+sed -i 's/^PS_USER/# PS_USER/' $FILE || echo 'PS_USER=root' >> $FILE
+sed -i 's/^PS_ARGUMENTS/# PS_ARGUMENTS/' $FILE || echo 'PS_ARGUMENTS="-p $PS_LISTENPORT -s /root/portspoof/tools/portspoof_signatures"' >> $FILE
 read -p "Ports to redirect to portspoof (eg. 1:21 23:79 81:65535): " -r PORTS
-grep -qoP '^(#\s*)?PS_UNFILTEREDPORTS' $FILE && sed -i -E 's/^(#\s*)?PS_UNFILTEREDPORTS.*/PS_UNFILTEREDPORTS=\"'"$PORTS"'\"/' $FILE || echo 'PS_UNFILTEREDPORTS=\"'"$PORTS"'\"' >> $FILE
+sed -i 's/^PS_UNFILTEREDPORTS/# PS_UNFILTEREDPORTS/' $FILE || echo "PS_UNFILTEREDPORTS=\"$PORTS\"" >> $FILE # Note the double-quotes here
 if [ "$OS_TYPE" == "debian" ]
 then
   update-rc.d portspoof defaults
